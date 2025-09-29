faea5635412de38599fe5dde017bda5cc282b6f1
오승찬 <oseungchan@oseungchan-ui-MacBookAir.local>
2025-09-29 22:14:58 +0900

    Feat: google 소셜 로그인 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,38 @@
 "use client";
+
 import { supabase } from "@/lib/supabaseClient";
 import { useEffect } from "react";
 
 export default function Home() {
-  async function getData() {
-    const { data: movies, error } = await supabase.from("movies").select("*");
-    console.log("데이터베이스로부터 받아온 movies", movies);
-  }
+  const signInWithGoogle = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+
+    if (error) {
+      console.error("Google 로그인 에러:", error.message);
+    } else {
+      console.log("Google 로그인 시도 중:", data);
+    }
+  };
 
   useEffect(() => {
-    getData();
+    const { data: subscription } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        console.log("Auth Event:", event);
+        console.log("Session:", session);
+      }
+    );
+
+    return () => {
+      subscription.subscription.unsubscribe();
+    };
   }, []);
-  return <div></div>;
+
+  return (
+    <main>
+      <h1>Google 로그인 테스트</h1>
+      <button onClick={signInWithGoogle}>구글로 로그인하기</button>
+    </main>
+  );
 }
